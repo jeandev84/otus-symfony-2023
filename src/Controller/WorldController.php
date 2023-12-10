04f95fdd0afcabb51419a4f4fb78d3c7af4b96f1
Jean-Claude <jeanyao@ymail.com>
2023-12-10 21:34:51 +0300

    Create UserBuilderService

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -1,39 +1,23 @@
 <?php
-declare(strict_types=1);
-
 namespace App\Controller;
 
-
-use App\Manager\UserManager;
+use App\Service\UserBuilderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * Created by PhpStorm at 10.12.2023
- *
- * Class WorldController
- *
- * @author Jean-Claude <jeanyao@ymail.com>
- *
- * @package App\Controller
- */
 class WorldController extends AbstractController
 {
+    public function __construct(private readonly UserBuilderService $userBuilderService)
+    {
+    }
 
-       protected UserManager $userManager;
+    public function hello(): Response
+    {
+        $user = $this->userBuilderService->createUserWithTweets(
+            'J.R.R. Tolkien',
+            ['The Hobbit', 'The Lord of the Rings']
+        );
 
-
-       public function __construct(UserManager $userManager)
-       {
-           $this->userManager = $userManager;
-       }
-
-
-
-       public function hello(): Response
-       {
-           $user = $this->userManager->create('My user');
-
-           return $this->json($user->toArray());
-       }
+        return $this->json($user->toArray());
+    }
 }
