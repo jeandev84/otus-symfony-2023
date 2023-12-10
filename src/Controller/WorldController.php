be65c3985d0ab8d44a669bc353952c1003a810f9
Jean-Claude <jeanyao@ymail.com>
2023-12-10 18:04:16 +0300

    WorldController

---
+++ src/Controller/WorldController.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Created by PhpStorm at 10.12.2023
+ *
+ * @WorldController
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @package App\Controller
+ */
+class WorldController
+{
+       public function hello(): Response
+       {
+           return new Response('<html><body><h1><b>Hello,</b> <i>world</i>!!!!</h1></body></html>');
+       }
+}
